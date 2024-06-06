30d79e6e0a1f430d7ab364c144683a66d7f81c7a
kapoorlab <randomaccessiblekapoor@gmail.com>
2024-06-06 18:20:47 +0200

    add alex for eco

--- src/lucid_kietzmannlab/modelzoo/vision_models.py
+++ src/lucid_kietzmannlab/modelzoo/vision_models.py
@@ -232,3 +232,72 @@ AlexNet.layers = _layers_from_list_of_dicts(
         {"tags": ["dense"], "name": "Softmax", "depth": 1000},
     ],
 )
+
+
+class AlexNetEcoset(Model):
+    """Original AlexNet weights ported to TF.
+
+    AlexNet is the breakthrough vision model from Krizhevsky, et al (2012):
+    https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf
+    This implementation is a caffe re-implementation:
+    http://www.cs.toronto.edu/~guerzhoy/tf_alexnet/
+    It was converted to TensorFlow by this GitHub project:
+    https://github.com/huanzhang12/tensorflow-alexnet-model
+    It appears the parameters are the actual original parameters.
+    """
+
+    # The authors of code to convert AlexNet to TF host weights at
+    # http://jaina.cs.ucdavis.edu/datasets/adv/imagenet/alexnet_frozen.pb
+    # but it seems more polite and reliable to host our own.
+    model_path = "gs://modelzoo/vision/other_models/AlexNet.pb"
+    labels_path = "gs://modelzoo/labels/ImageNet_standard.txt"
+    synsets_path = "gs://modelzoo/labels/ImageNet_standard_synsets.txt"
+    dataset = "ImageNet"
+    image_shape = [227, 227, 3]
+    is_BGR = True
+    image_value_range = (-IMAGENET_MEAN_BGR, 255 - IMAGENET_MEAN_BGR)
+    input_name = "Placeholder"
+
+    model_checkpoint_path = "model.ckpt_epoch89"
+    model_checkpoint_dir = "/path/to/checkpoint/directory"
+
+    AlexNet.layers = _layers_from_list_of_dicts(
+        AlexNet(),
+        [
+            {"tags": ["pre_relu", "conv"], "name": "Conv2D", "depth": 96},
+            {"tags": ["pre_relu", "conv"], "name": "Conv2D_1", "depth": 128},
+            {"tags": ["pre_relu", "conv"], "name": "Conv2D_2", "depth": 128},
+            {"tags": ["pre_relu", "conv"], "name": "Conv2D_3", "depth": 384},
+            {"tags": ["pre_relu", "conv"], "name": "Conv2D_4", "depth": 192},
+            {"tags": ["pre_relu", "conv"], "name": "Conv2D_5", "depth": 192},
+            {"tags": ["pre_relu", "conv"], "name": "Conv2D_6", "depth": 128},
+            {"tags": ["pre_relu", "conv"], "name": "Conv2D_7", "depth": 128},
+            {"tags": ["dense"], "name": "Relu", "depth": 4096},
+            {"tags": ["dense"], "name": "Relu_1", "depth": 4096},
+            {"tags": ["dense"], "name": "Softmax", "depth": 1000},
+        ],
+    )
+
+    def load_model_checkpoint(self, sess):
+        saver = tf.compat.v1.train.import_meta_graph(
+            f"{self.model_checkpoint_dir}/{self.model_checkpoint_path}.meta"
+        )
+        saver.restore(
+            sess, f"{self.model_checkpoint_dir}/{self.model_checkpoint_path}"
+        )
+
+    def __init__(self):
+        self._graph_def = None
+
+    @property
+    def graph_def(self):
+        if not self._graph_def:
+            # Load the model checkpoint and return the graph_def
+            with tf.compat.v1.Graph().as_default() as graph:
+                with tf.compat.v1.Session() as sess:
+                    self.load_model_checkpoint(sess)
+                    self._graph_def = graph.as_graph_def()
+        return self._graph_def
+
+    def post_import(self, scope):
+        pass
